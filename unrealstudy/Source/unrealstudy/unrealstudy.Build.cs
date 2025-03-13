94d1a1da9708b3ca0caef2678511df10d5b7c581
leehuchan <dmzdmz001@naver.com>
2025-03-13 18:04:24 +0900

    New fire asset & New character asset

--- unrealstudy/Source/unrealstudy/unrealstudy.Build.cs
+++ unrealstudy/Source/unrealstudy/unrealstudy.Build.cs
@@ -8,7 +8,7 @@ public class unrealstudy : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "AIModule", "NavigationSystem", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "AIModule", "NavigationSystem", "GameplayTasks", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
